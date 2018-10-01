597084dc90da0ff0ed6dad5e272478e1eea41e7f
Антон Красильщиков <a.krasilshchikov@rambler-co.ru>
2018-10-01 14:15:26 +0300

    naming issue

--- src/Toxu4.AutoFixture.LimitDepth.Lib/AutoFixtureExtensions.cs
+++ src/Toxu4.AutoFixture.LimitDepth.Lib/AutoFixtureExtensions.cs
@@ -5,11 +5,11 @@ namespace Toxu4.AutoFixture.LimitDepth.Lib
 {
     public static class AutoFixtureExtensions
     {
-        public static Fixture WithLimitedDepth(this Fixture fixture, int depthLimit = 2)
+        public static Fixture WithLimitedDepth(this Fixture fixture, int maxDepth = 2)
         {
             fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
                 .ForEach(b => fixture.Behaviors.Remove(b));
-            fixture.Behaviors.Add(new LimitDepth(depthLimit));
+            fixture.Behaviors.Add(new LimitDepth(maxDepth));
             
             return fixture;
         }
